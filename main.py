99969dd8ed046d6040085edbbc44c451954b8baf
wiggapony0925 <ninjeff06@gmail.com>
2023-04-23 12:09:44 -0400

    tittle

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import PySimpleGUI as pg
 
 #WINDOW CREATION 
 pg.theme('DarkAmber')
-pg.Window(title="AUTO CLICKER", layout=[[]], margins=(250,350)).read()
+#pg.Window(title="AUTO CLICKER", layout=[[]], margins=(250,350)).read()
+
+layout = [  [pg.Text('Auto Clicker <3', size=(20, 2), justification='center')]]
