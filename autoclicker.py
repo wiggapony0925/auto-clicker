17b545053fb93bf5b9e9b8ae9a4d0f3985d68d51
wiggapony0925 <ninjeff06@gmail.com>
2023-04-23 23:58:32 -0400

    auto clicker modules

--- autoclicker.py
+++ autoclicker.py
@@ -1,6 +1,7 @@
-import pyautogui
-import time
+import time 
+import threading 
+from typing import get_args
+from pynput.mouse import Button, Controller 
+from pynput.keyboard import Listener, KeyCode
 
-class AutoClicker:
-    def __init__(Self):
-        pass
+
