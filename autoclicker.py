e061ffa885544b2de45e7bb76ac9b75608f6abe2
wiggapony0925 <ninjeff06@gmail.com>
2023-04-24 22:45:43 -0400

    var

--- autoclicker.py
+++ autoclicker.py
@@ -4,4 +4,10 @@ from typing import get_args
 from pynput.mouse import Button, Controller 
 from pynput.keyboard import Listener, KeyCode
 
+#some
+delay = 0.1 
+button = Button.left 
+start_stop_key = KeyCode(char='r') #ipnut for gui 
+
+
 
